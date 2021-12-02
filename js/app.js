3a2a1c631bf029bcce039b572c83ef7d5e25c831
Umidjon <umidzhonofficialdev@gmail.com>
2021-12-02 16:36:14 +0600

    Typing Effect With Vanilla Js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
-const dynamicText = document.querySelector("#dynamic__text");
-const dynamicTextContent = dynamicText.textContent.split("")
-const dynamicTextContentClone = [...dynamicTextContent];
-const dynamicTextContentClone2 = [...dynamicTextContent];
-let countDelete = 0;
-let countAdd = 0;
+this.addEventListener("load",() =>{
+    let speed = 100;
+    function set() {
+        const dynamicText = document.querySelector("#dynamic__text");
+        const dynamicTextContent = dynamicText.textContent.split("")
+        const dynamicTextContentClone = [...dynamicTextContent];
+        let countDelete = 0;
+        let countAdd = 0;
+        let typingInterval = setInterval(() => {
+            typing()
+        }, speed)
+        
+        function typing() {
+            if (countDelete < dynamicTextContent.length) {
+                typingDelete()
+            } else {
+                typingAdd()
+            }
+        }
+        function typingDelete() {
+            dynamicTextContentClone.splice(-1, 1)
+            dynamicText.textContent = dynamicTextContentClone.join("")
+            countDelete++
+        }
+        function typingAdd() {
+            if (countAdd < dynamicTextContent.length) {
+                dynamicText.textContent += dynamicTextContent[countAdd];
+                countAdd++;
+            } else {
+                clearInterval(typingInterval)
+            }
+        }
+    }
 
-this.onload = () =>{
-    let typingInterval = setInterval(() => {
-        typing()
-    }, 200)
-    
-    function typing() {
-        if (countDelete < dynamicTextContent.length) {
-            typingDelete()
-        } else {
-            typingAdd()
-        }
-    }
-    
-    function typingDelete() {
-        dynamicTextContentClone.splice(-1, 1)
-        dynamicText.textContent = dynamicTextContentClone.join("")
-        countDelete++
-    }
-    
-    function typingAdd() {
-        if (countAdd < dynamicTextContent.length) {
-            dynamicText.textContent += dynamicTextContent[countAdd];
-            countAdd++;
-        } else {
-            clearInterval(typingInterval)
-        }
-    }
-}
+    let time = (Array.from(document.querySelector("#dynamic__text").textContent).length + 2) * speed *2
+
+
+    setInterval(set,time)
+})
